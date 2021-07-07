1032df08aaffe53bd80d2be8c4668f2ba909b3c0
Zak Smith <zsmith3.dev@gmail.com>
2021-07-07 14:35:50 +0100

    Add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,31 @@ import React from 'react';
 import './App.css';
 import NasaInfo from "./components/NasaInfo";
 import ButtonCounter from "./components/ButtonCounter";
+import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
 
-function App() {
+export default function App() {
     return (
-        <div className="App">
-            <main>
-                <NasaInfo/>
-                <ButtonCounter/>
+        <BrowserRouter>
+            <header>
+                <div>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/nasa">NASA</Link></li>
+                        <li><Link to="/counter">Counter</Link></li>
+                    </ul>
+                </div>
+            </header>
+
+            <main className="App">
+                <Switch>
+                    <Route path="/nasa">
+                        <NasaInfo/>
+                    </Route>
+                    <Route path="/counter">
+                        <ButtonCounter/>
+                    </Route>
+                </Switch>
             </main>
-        </div>
+        </BrowserRouter>
     );
 }
-
-export default App;
