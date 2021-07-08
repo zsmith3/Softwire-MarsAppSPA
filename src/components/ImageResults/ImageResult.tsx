d17b91a3eb875f995d9632eb5f2b0b41be65297d
Jordan Kisa <jordan.kisa@softwire.com>
2021-07-08 14:50:21 +0100

    Image results creates image element for entire photo list

--- src/components/ImageResults/ImageResult.tsx
+++ src/components/ImageResults/ImageResult.tsx
@@ -1,32 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import Image from "./Image";
-import axios from "axios"
+import Photo from "../../dataClasses/Photo";
 
-async function getRoverImages(rover : string, cameraType : string) {
-    const url = 'http://localhost:8000/rovers/' + rover + '/photos/' + cameraType;
-    console.log(url)
-    try {
-        return await axios.get(url)
-    } catch (error) {
-        let errorStr: string;
-        if (error.response && error.response.data && error.response.data.errors) errorStr = error.response.data.errors;
-        else errorStr = error.toString();
-        throw {status: error.response.status, message: `Rover Image API error: ${errorStr}`};
-    }
-}
+export default function ImageResult(props: {PhotoList: Photo[]}) {
+    console.log(props.PhotoList)
 
-export default  async function ImageResult() {
-    let rover = "curiosity"
-    let camera = "fhaz"
-    let roverResponse =  getRoverImages(rover, camera)
     return (
-        <div className="Image Result">
-            These are the Image results
-            <Image/>
-            <Image/>
-            <Image/>
-            <Image/>
-            <Image/>
+        <div className="ImageResult">
+            <h1>Rover Image Search Results</h1>
+            <br/>
+            {props.PhotoList.map(photo => <Image photo={photo}/>)}
         </div>
     )
 
