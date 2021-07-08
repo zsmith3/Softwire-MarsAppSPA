429313b0eabc4b2af955c8ff4c94c54a655e04ab
Zak Smith <zsmith3.dev@gmail.com>
2021-07-08 16:09:37 +0100

    Fix CSS

--- src/components/MarsPhotos/ImageResult.tsx
+++ src/components/MarsPhotos/ImageResult.tsx
@@ -6,11 +6,14 @@ export default function ImageResult(props: { photoList: (Photo[] | null) }) {
     return (
         <div className="ImageResult">
             <h3>Results</h3>
-            <div className="imageList">
-                {props.photoList ? (props.photoList.length ? props.photoList.map(photo => <Image key={photo.id}
-                                                                                                 photo={photo}/>) :
-                    <p>No Images Found</p>) : <p>Please select search parameters.</p>}
-            </div>
+                {props.photoList ?
+                    (props.photoList.length ?
+                        <div className="imageList">
+                            {props.photoList.map(photo => <Image key={photo.id} photo={photo}/>)}
+                        </div> :
+                        <p>No Images Found</p>) :
+                    <p>Please select search parameters.</p>
+                }
         </div>
     )
 }
